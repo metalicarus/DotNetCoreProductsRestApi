df36082a8ea8c36bc63ce4ef7678a0b9a1af0a83
metalicarus <silva.matheusms.mateus83@gmail.com>
2021-10-02 17:21:33 -0300

    new migrations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DotNetRestApi.Models
 {
+    [Table("Categories")]
     public class Category
     {
 
@@ -11,8 +14,15 @@ namespace DotNetRestApi.Models
             Products = new Collection<Product>();
         }
 
+        [Key]
         public int Id {get; set;}
+
+        [Required]
+        [MaxLength(32)]
         public string Name {get; set;}
+
+        [Required]
+        [MaxLength(250)]
         public string ImageUrl {get; set;}
         public ICollection<Product> Products {get ;set; }
     }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,13 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DotNetRestApi.Models
 {
+    [Table("Products")]
     public class Product
     {
+        [Key]
         public int Id {get; set;}
+
+        [Required]
+        [MaxLength(30)]
         public string Name {get; set;}
+
+        [Required]
+        [MaxLength(50)]
         public string Description {get; set;}
         public decimal Price {get; set;}
+
+        [Required]
+        [MaxLength(250)]
         public string ImageUrl {get; set;}
         public float stock {get; set;}
         public DateTime CreatedAt {get; set;}
